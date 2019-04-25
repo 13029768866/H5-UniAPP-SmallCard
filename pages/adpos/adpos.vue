97105b134218d0803d3e3412c1fdc2452847060e
woodensRangerWZJ <13029768866@163.com>
2019-04-25 09:49:20 +0800

    金融圈--广告投放页面

---
+++ pages/adpos/adpos.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<checkbox-group>
+			<label>
+				<checkbox class="wzj" value="cb" checked="true" />选中
+			</label>
+			<label>
+				<checkbox value="cb" />未选中
+			</label>
+			
+		</checkbox-group>
+	</view>
+</template>
+<script>
+	export default{
+		
+	}
+</script>
+<style>
+/* #ifdef APP-PLUS || MP-WEIXIN  */
+checkbox .uni-checkbox-input {
+border-radius: 50%;
+}
+/* #endif */
+</style>
