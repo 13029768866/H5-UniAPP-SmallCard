7e776653437b9c0f6f5c8d8d777ea23c3e913a6a
woodensRangerWZJ <13029768866@163.com>
2019-05-16 09:48:46 +0800

    我的收益页面

---
+++ pages/myEarnings/myEarnings.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="myEarnings">
+		123
+	</view>
+</template>
+
+<script>
+</script>
+
+<style lang="stylus">
+// 隐藏滚动条
+scroll-view ::-webkit-scrollbar{
+	   width: 0;
+	   height: 0;
+	   color: transparent;
+}
+page, .myEarnings
+	position: relative;
+	width: 100%;
+	height: 100%;
+	background: rgba(245,247,251,1);;
+	overflow: hidden;
+</style>
